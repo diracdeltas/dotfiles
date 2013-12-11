c2284891c041a03c0dbf46e4b7ed8002a32e4655
Yan <yan@mit.edu>
2013-12-11 10:24:40 -0800

    better colors

---
+++ config.fish
@@ -0,0 +1,24 @@
+function vim-help
+    vim ~/vim-help.txt
+end
+
+function xmonad-help
+    vim ~/xmonad-help.txt
+end
+
+function tomsay
+    cowsay -f tom
+end
+
+function gtfo
+    gnome-session-quit
+end
+
+function gdisplay
+    gnome-control-center display
+end
+
+function tmux2
+    set TERM screen-256color-bce
+    tmux
+end
