d813fffd48eea81daf3bc098c3765df7b491a2c9
yan <yan@brave.com>
2018-10-05 17:59:16 -0700

    fish updates

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,3 +1,4 @@
 alias autobrew "brew update; and brew outdated | awk ' {print $1} ' | xargs brew upgrade; and brew cleanup"
 alias macchange "sudo ifconfig en0 ether $MAC_ADDRESS"
 alias pythonserver "python3 -m http.server 9290 --bind 127.0.0.1"
+alias autoclean "rm -r ~/.electron; rm -r node_modules/electron-prebuilt"
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,3 @@
 . ~/.config/fish/aliases.fish
-set -gx PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin /usr/local/opt/ccache/libexec /Users/yan/.npm-packages/bin /Users/yan/.cargo/bin
+set -gx PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin /usr/local/opt/ccache/libexec /Users/yan/.npm-packages/bin /Users/yan/.cargo/bin ~/.npm-global/bin
 set -gx NODE_PATH /Users/yan/.npm-packages/lib/node_modules
